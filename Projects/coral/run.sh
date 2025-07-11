b0da814567f5476d46203b1f5952da180a8138b5
Shinya Kaneko <shinya.tim.com@gmail.com>
2025-07-11 14:03:16 +0900

    PPFDのinputコメントアウト

--- Projects/coral/run.sh
+++ Projects/coral/run.sh
@@ -35,7 +35,7 @@ mkdir -p output
 ./ecosys_test.exe < coral_bl_T25.in
 ./ecosys_test.exe < coral_bl_T33.in
 ./ecosys_test.exe < coral_bl_T13.in
-#./ecosys_test.exe < coral_bl_T17.in
-#./ecosys_test.exe < coral_bl_T21.in
-#./ecosys_test.exe < coral_bl_T29.in
+./ecosys_test.exe < coral_bl_T17.in
+./ecosys_test.exe < coral_bl_T21.in
+./ecosys_test.exe < coral_bl_T29.in
 #
